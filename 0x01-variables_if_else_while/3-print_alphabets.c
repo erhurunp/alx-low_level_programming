1990ac2c91a99b5a1ab034ad580cd22364cc03c3
erhurunp <erhurunp@gmail.com>
2022-03-11 09:45:57 -0800

    Task Three

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,10 +15,10 @@ int main(void)
 	{
 		putchar(ch);
 	}
-	for (ch = 'A' ch <= 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 	{
 		putchar(ch);
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
