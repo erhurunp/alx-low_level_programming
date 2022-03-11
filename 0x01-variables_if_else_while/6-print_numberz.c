aacaed70d829849435fb090a164ead61acff3163
erhurunp <erhurunp@gmail.com>
2022-03-11 11:33:04 -0800

    Task Six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers in base ten
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
