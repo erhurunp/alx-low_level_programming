538f11370d9420eaae12af63f346cb13f5aaaeec
erhurunp <erhurunp@gmail.com>
2022-03-11 07:06:12 -0800

    Task One

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	} else if (n == 0)
 	{
 		printf("Last digit of n and is 0\n");
-	} else if (n != 0 && < 6)
+	} else if (n < 6 && n != 0)
 	{
 		printf("Last digit of n is less than 6 and not 0\n");
 	}
