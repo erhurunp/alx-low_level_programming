524d836333806b8e4c567c0757482393d14201d2
erhurunp <erhurunp@gmail.com>
2022-03-11 09:59:27 -0800

    Task Four

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the letters of the alphabets
+ * except q and e, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' && ch == 'q')
+		{
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
