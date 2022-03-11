4dbbb824e155ff6af08dd086d3e610a495d3045d
erhurunp <erhurunp@gmail.com>
2022-03-11 09:41:43 -0800

    Task Three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints out letters of the aiphabets
+ * in lowercase, then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+	return (0);
+}
