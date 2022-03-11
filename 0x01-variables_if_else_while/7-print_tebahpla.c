28ee1cdb10dbebb9434afb30d5f88e5ebf47e507
erhurunp <erhurunp@gmail.com>
2022-03-11 11:46:07 -0800

    Task Seven

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the letters of the alphabet
+ * in lowercase but in reverse order,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
