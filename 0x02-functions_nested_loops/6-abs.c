02670cd6073b308f4c8e46f4d659dfaec278e0dd
erhurunp <erhurunp@gmail.com>
2022-04-26 05:39:10 -0700

    Task Six

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of
+ * an integer
+ * @r: r is an integer
+ *
+ * Return: an integer
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-r);
+	}
+	if (r > 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r);
+	}
+}
