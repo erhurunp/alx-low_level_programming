01951256f9adc27f8b981d71781695420af21a04
erhurunp <erhurunp@gmail.com>
2022-04-26 09:48:23 -0700

    Task Twelve

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,18 +14,14 @@ void print_times_table(int n)
 
 	if (n >= 0 && n < 15)
 	{
-		horizontal = 0;
-		while (horizontal <= 9)
+		for (horizontal = 0; horizontal <= n; horizontal++)
 		{
-			vertical = 0;
-			while (vertical <= 9)
+			for (vertical = 0; vertical <= n; vertical++)
 			{
 				product = horizontal * vertical;
 
 				if (vertical == 0)
-				{
 					_putchar('0');
-				}
 				else if (product < 10)
 				{
 					_putchar(',');
@@ -38,14 +34,20 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(product / 100 + '0');
 					_putchar((product / 10) % 10 + '0');
 					_putchar(product % 10 + '0');
 				}
-				vertical++;
 			}
 			_putchar('\n');
-			horizontal++;
 		}
 	}
 }
