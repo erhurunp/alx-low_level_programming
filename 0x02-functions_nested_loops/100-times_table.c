1890505b22a576621005005a28d430e19e6d1ae5
erhurunp <erhurunp@gmail.com>
2022-04-26 09:33:22 -0700

    Task Twelve

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * times_table - prints n multiplication
+ * table starting with zero
+ * @n: n is an integer
+ *
+ * Return: void
+ */
+
+void times_table(int n)
+{
+	int horizontal, vertical;
+
+	if (n >= 0; && n < 15)
+	{
+		horizontal = 0;
+		while (horizontal <= 9)
+		{
+			vertical = 0;
+			while (vertical <= 9)
+			{
+				product = horizontal * vertical;
+
+				if (vertical == 0)
+				{
+					_putchar('0');
+				}
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				vertical++;
+			}
+			_putchar('\n');
+			horizontal++;
+		}
+	}
+}
