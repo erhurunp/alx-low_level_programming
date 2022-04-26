82e38ba67c978ee35b90b65ab2a9beb8aef2ab60
erhurunp <erhurunp@gmail.com>
2022-04-26 04:43:40 -0700

    Task Four

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,7 @@ include "main.h"
 /**
  * _isalpha - checks for uppercase and
  * lowercase letters
+ * @c: c is in ascii
  *
  * Return: 1 if character is either an
  * uppercase or lowercase
@@ -11,9 +12,15 @@ include "main.h"
 int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return 1;
+	{
+		return (1);
+	}
 	else if (c >= 'A' && c <= 'Z')
-		return 1;
+	{
+		return (1);
+	}
 	else
-		return 0;
+	{
+		return (0);
+	}
 }
