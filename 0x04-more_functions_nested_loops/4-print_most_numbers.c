50166ba62a74b8044f8acc02f30b427af8d63123
erhurunp <erhurunp@gmail.com>
2022-04-28 11:18:33 -0700

    Task Four

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers within a
+ * certain range (0 - 9)
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		if (c == 2 || c == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c + '0');
+		}
+		c++;
+	}
+	_putchar('\n');
+}
