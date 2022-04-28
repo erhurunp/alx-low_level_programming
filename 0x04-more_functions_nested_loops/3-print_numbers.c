92cdd0c49a3492731fe572add88cdd7f370d7f80
erhurunp <erhurunp@gmail.com>
2022-04-28 11:06:00 -0700

    Task Three

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 - 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		_putchar(i + '0');
+		c++;
+	}
+	_putchar('\n');
+}
