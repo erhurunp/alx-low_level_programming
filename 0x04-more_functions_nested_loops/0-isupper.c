9781db47d2ba9d709ca41a4fccfec1dedd2f57e9
erhurunp <erhurunp@gmail.com>
2022-04-28 07:41:45 -0700

    Task Zero

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if c is uppercase
+ * @c: c is an ascii character
+ *
+ * Return: 1 (if uppecase), 0 (otherwise)
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# More Functions And Loops
